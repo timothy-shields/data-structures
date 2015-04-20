ab2be7417d63740a35b1b5f27b8af213f5cc8637
Timothy Shields <timothy.shields@live.com>
2015-04-20 01:42:51 -0400

    Added initial tests.

---
+++ Shields.DataStructures.Tests/PairingHeapTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shields.DataStructures.Tests
+{
+    [TestClass]
+    public class PairingHeapTests : PriorityQueueTests
+    {
+        protected override IPriorityQueue<int, string> CreatePriorityQueue()
+        {
+            return new PairingHeap<int, string>();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorOnNullComparerThrows()
+        {
+            new PairingHeap<int, string>(null);
+        }
+    }
+}
---
+++ Shields.DataStructures.Tests/PriorityQueueTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shields.DataStructures.Tests
+{
+    [TestClass]
+    public abstract class PriorityQueueTests
+    {
+        protected abstract IPriorityQueue<int, string> CreatePriorityQueue();
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void GetMinOnEmptyPriorityQueueThrows()
+        {
+            var q = CreatePriorityQueue();
+            q.GetMin();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RemoveNullHandleThrows()
+        {
+            var q = CreatePriorityQueue();
+            q.Remove(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void RemoveOnWrongInstanceThrows()
+        {
+            var q1 = CreatePriorityQueue();
+            var q2 = CreatePriorityQueue();
+            q2.Remove(q1.Add(0, null));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void UpdateKeyOnNullHandleThrows()
+        {
+            var q = CreatePriorityQueue();
+            q.Remove(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void UpdateKeyOnWrongInstanceThrows()
+        {
+            var q1 = CreatePriorityQueue();
+            var q2 = CreatePriorityQueue();
+            q2.UpdateKey(q1.Add(0, null), 0);
+        }
+    }
+}
