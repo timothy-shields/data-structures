60d4b1dfca38a1b264e2bb4c5ebf826de833b92a
Timothy Shields <timothy.shields@live.com>
2015-04-20 23:01:00 -0400

    Remove comment that applied to GetMin method.

--- Shields.DataStructures/PairingHeap.cs
+++ Shields.DataStructures/PairingHeap.cs
@@ -71,7 +71,6 @@ namespace Shields.DataStructures
         /// Gets a handle with a minimal key.
         /// If the priority queue is empty, an <see cref="InvalidOperationException"/> is thrown.
         /// </summary>
-        /// <returns>A handle with a minimal key.</returns>
         /// <exception cref="System.InvalidOperationException">Thrown if the priority queue is empty.</exception>
         public IPriorityQueueHandle<TKey, TValue> Min
         {
